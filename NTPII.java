e4a92629083ccf2e157d98455f0412facee76a41
Serhat Ergül <91741288+Dayicim@users.noreply.github.com>
2021-10-17 01:22:09 +0300

    Update NTPII.java

--- NTPII.java
+++ NTPII.java
@@ -1,15 +1,11 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package ntp.ı;
 
 import java.util.Scanner;
 
-/**
- *
- * @author Serhat
- */
+
+ @author Serhat
+
 public class NTPII {
 
     public static void main(String[] args) {
